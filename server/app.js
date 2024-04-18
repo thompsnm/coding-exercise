f47b58bad711049fae026a82458cb2ab533e60d6
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2024-04-18 12:36:40 -0600

    Create dev DB and campaigns api endpoint

--- server/app.js
+++ server/app.js
@@ -1,13 +1,19 @@
 const path = require('path');
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3500;
+const db = require('./models/db');
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.send({ message: "Hello from Express!" });
+app.get('/api', (req, res) => {
+    res.send({ message: 'Hello from Express!' });
+});
+
+app.get('/campaigns', async (req, res) => {
+    let campaigns = await db.Campaign.findAll();
+    res.send(campaigns);
 });
 
 // Handle React routing, return all requests to React app
