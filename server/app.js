62ad16fab93e4dba8645622b6ea45fdead366628
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2024-04-18 13:00:03 -0600

    Add initial line item API endpoints

--- server/app.js
+++ server/app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3500;
 const db = require('./models/db');
+const qs = require('qs');
 
 // Serve any static files
 app.use(express.static(path.join(__dirname, '../client/build')));
+app.set('query parser', function (str) {
+    return qs.parse(str);
+});
 
 app.get('/api', (req, res) => {
     res.send({ message: 'Hello from Express!' });
@@ -18,13 +22,35 @@ app.get('/campaigns', async (req, res) => {
 
 app.get('/campaign/:id', async (req, res) => {
     let campaign = await db.Campaign.findOne({
-      where: { id: req.params.id }
+        where: { id: req.params.id }
     });
 
     if (campaign === null) {
-      res.status(404).send("Campaign not found with id: " + req.params.id);
+        res.status(404).send("Campaign not found with id: " + req.params.id);
     } else {
-      res.send(campaign);
+        res.send(campaign);
+    }
+});
+
+app.get('/lineItems', async (req, res) => {
+    //let clause =
+        //!!req.query.campaign_id
+        //? { where: { campaign_id: req.query.campaign_id} }
+        //: {};
+
+    let lineItems = await db.LineItem.findAll();
+    res.send(lineItems);
+});
+
+app.get('/lineItem/:id', async (req, res) => {
+    let lineItems = await db.LineItem.findOne({
+        where: { id: req.params.id }
+    });
+
+    if (lineItems === null) {
+        res.status(404).send("Line Item not found with id: " + req.params.id);
+    } else {
+        res.send(lineItems);
     }
 });
 
