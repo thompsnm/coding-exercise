9cc14a5d758dae0832ac91b283ac4af4c9902fa5
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2024-04-18 12:51:59 -0600

    Add campaign detail api endpoint

--- server/app.js
+++ server/app.js
@@ -16,6 +16,18 @@ app.get('/campaigns', async (req, res) => {
     res.send(campaigns);
 });
 
+app.get('/campaign/:id', async (req, res) => {
+    let campaign = await db.Campaign.findOne({
+      where: { id: req.params.id }
+    });
+
+    if (campaign === null) {
+      res.status(404).send("Campaign not found with id: " + req.params.id);
+    } else {
+      res.send(campaign);
+    }
+});
+
 // Handle React routing, return all requests to React app
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
