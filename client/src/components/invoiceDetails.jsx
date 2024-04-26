d9d070a70e1e350baf16cdda5e28d4d7f7427aba
Nathan Thompson <Nathan.Mike.Thompson@gmail.com>
2024-04-26 14:47:02 -0600

    Add some IDs for testing

--- client/src/components/invoiceDetails.jsx
+++ client/src/components/invoiceDetails.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function InvoiceDetails({ invoiceDetails }) {
     return (
         <div>
-            <p>Campaign ID: { invoiceDetails.campaign_id }</p>
-            <p>Booked Amount: { invoiceDetails.booked_amount }</p>
-            <p>Actual Amount: { invoiceDetails.actual_amount }</p>
-            <p>Adjustments: { invoiceDetails.adjustments }</p>
+            <p id="campaign_id">Campaign ID: { invoiceDetails.campaign_id }</p>
+            <p id="booked_amount">Booked Amount: { invoiceDetails.booked_amount }</p>
+            <p id="actual_amount">Actual Amount: { invoiceDetails.actual_amount }</p>
+            <p id="adjustments">Adjustments: { invoiceDetails.adjustments }</p>
         </div>
     );
 }
